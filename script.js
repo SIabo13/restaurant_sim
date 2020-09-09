40aceb7fe832f832cc1558a6dd392799d9dbfdee
Raul Rodriguez <raulrodriguez@Rauls-MacBook-Pro.local>
2020-09-08 21:19:22 -0400

    Added modals for store, hire, and menu. Created methods to populate lists and objects for data

--- script.js
+++ script.js
@@ -38,11 +38,38 @@
         customerOrders:[],
         ordersBeingProcess:[],
         ordersCompleted:[],
-        employees: [],
-
-        meals:[
-            
+        employees: [
+            {
+                name:'Tom',
+                lvl: 5,
+                job:'Chef'
+            },
+            {
+                name:'Krystal',
+                lvl: 13,
+                job:'Prep'
+            },
+            {
+                name:'Charles',
+                lvl:23,
+                job:'Chef'
+            }
         ],
+
+        recipes:{
+            burger:{
+                ingredients:['1 set of buns','1 beef patty','1 lettuce leaf']
+            },
+            spaghetti:{
+                ingredients:['1 pack of noodles','4 meatballs',"10 fl oz"]
+            },
+            chickenTenderMeal:{
+                ingredients:["6 chicken tenders","1 pack"]
+            },
+            steakDinner:{
+                ingredients:["10 oz steak","1 potato","3 cloves"]
+            }
+        },
 
         store:{
             potato:35,
@@ -51,6 +78,19 @@
             buns: 2,
             beefPatties: 9,
             lettuce: 2
+        },
+        orders:{
+            burger:{
+                ingredients:['beef patty', 'buns','lettuce'],
+                prepTime: 30,
+                cookTime: 30
+                
+            },
+            steakDinner:{
+                ingredients:['beef steak','potato','broccoli'],
+                prepTime: 10,
+                cookTime:60
+            }
         }
         }
 
@@ -64,10 +104,11 @@
     let DOM = {
         money:document.querySelector("#money"),
         inventoryList: document.querySelector("#inventory"),
-        employees: document.querySelector("#employees"),
+        employeeList: document.querySelector("#employee"),
         customerOrders:document.querySelector("#customer_orders"),
         ordersProcessed:document.querySelector("#orders_processed"),
-        startDay:document.querySelector("#start-day")
+        startDay:document.querySelector("#start-day"),
+        menu:document.querySelector("#menu")
         }
         return {
             DOM:DOM
@@ -81,10 +122,52 @@
     const addInventory = () =>{
         let html;
         for (const inventory in data.inventory) {
-            html = `<li class="list-group-item">${inventory}: <span>${data.inventory[inventory]}</span></li>`;
+            html = `<li class="list-group-item">${inventory}: <span class="level">QTY: ${data.inventory[inventory]}</span></li>`;
             dom.inventoryList.innerHTML += html;
           }
         
+    }
+
+    const addRecipe = () =>{
+        // let html;
+        // data.employees.forEach(element => {
+        //     console.log(element['name'] + ' ' + element['lvl'] + ' ' + element['job'])
+        //     html = `<li class="list-group-item">${element['job']}: ${element['name']} <span class="level">LVL: ${element['lvl']}</span></li>`;
+        //    dom.menu.innerHTML += html;
+        // });
+    }
+
+    const addEmployee = () =>{
+        let html;
+        data.employees.forEach(element => {
+            console.log(element['name'] + ' ' + element['lvl'] + ' ' + element['job'])
+            html = `<li class="list-group-item">${element['job']}: ${element['name']} <span class="level">LVL: ${element['lvl']}</span></li>`;
+           dom.employeeList.innerHTML += html;
+        });
+
+    }
+
+    const customerOrders = () => {
+        /* 
+        - Customers choose order index from orders object and pushes it into list
+
+        - Customer appends random countdown to order object that count downs.
+
+        - If order is completed before countdown then the time remaining is rated against percentage
+        If above 70% payment is high, if above 40 payment is mediumm, if below 40 payment is low. if percentage is 0 no payment is recieved
+        */
+       ordersBeingProcess();
+    }
+
+    const ordersBeingProcess = (orders) => {
+        // Orders are pushed from customerOrder and processes by chef and preps
+        //Orders are passed to ordersCompleted
+
+        ordersCompleted();// Competed orders are passed as argument to ordersCompleted
+    }
+
+    const ordersCompleted = () =>{
+
     }
 
     const buyInventory = () => {
@@ -93,16 +176,19 @@
 
     const events = () => {
         console.log('Day has started');
+        customerOrders();
+        
         
         
     }
 
     const init = () =>{
         addInventory();
+        addEmployee();
     }
     
     
-    dom.startDay.addEventListener('click', events)
+    dom.startDay.addEventListener('click', events);
 
     return{
         init:init
